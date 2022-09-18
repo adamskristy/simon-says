3b19d97aa8c3c9cc3b90f9ef5c3e516f516429ca
Kristy Adams <kladams128@gmail.com>
2022-09-18 10:32:05 -0400

    testing and updating code

--- js/test2.js
+++ js/test2.js
@@ -1,14 +1,15 @@
 let sequence = []; //order pattern flashes
-let playerSequence = []; //order player clicks
-let turn; //computer turn
+let playerOneSequence = []; //order player1 clicks
+let playerTwoSequence = []; //order player2 clicks
+let turn; //whose turn it is
 let on; //power button on
 let level = 0; //what level
-let strike = false;
+let strike = false; // if player messed up
 
 
 
 //grabbing html elements to reference in JS
-const turnCounter = document.querySelector('#turn');
+const levelCounter = document.querySelector('#level');
 const greenButton = document.querySelector('#green');
 const redButton = document.querySelector('#red');
 const yellowButton = document.querySelector('#yellow')
@@ -25,24 +26,127 @@ const tileContainer = document.querySelector('.container')
 onButton.addEventListener('click', (event) => {
     if (onButton.checked == true){
         on = true;
-        turnCounter.innerHTML = "-";
+        levelCounter.innerHTML = "-";
         strikeCounter.innerHTML = "-";
+        console.log('power on')
     } else {
         on = false;
-        turnCounter.innerHTML = "";
+        levelCounter.innerHTML = "";
         strikeCounter.innerHTML = "";
-        resetGame();
     }
 })
 
+
+function resetGame() {
+    sequence = [];
+    playerOneSequence = [];
+    playerTwoSequence = [];
+    level = 0;
+    strike = false;
+    levelCounter.innerHTML = "-";
+    strikeCounter.innerHTML = "-";
+}
+
+//indicates player turn
+function playerTurn() {
+    console.log('player turn')
+    tileContainer.classList.remove('unclickable')
+
+    if (turn == 1) {
+        console.log('p1 turn')
+        strikeCounter.innerHTML = "P1";
+        playerOneClick();
+    } else if (turn == 2) {
+        console.log('p2 turn')
+        strikeCounter.innerHTML = "P2";
+        playerTwoClick();
+    }
+
+}
+
+//handleclick
+function playerTwoClick(tile){
+    turn = 2;
+    strikeCounter.innerHTML = "P2"; 
+    console.log('p2 click')
+
+    const index = playerTwoSequence.push(tile) - 1;
+
+    if (playerTwoSequence[index] !== sequence[index]) {
+        strike = true;
+        strikeCounter.innerHTML = "NO!"
+        resetGame();
+    }
+
+    if (playerTwoSequence.length === sequence.length) {
+        if (playerTwoSequence.length === 20 && strike == false){
+            resetGame();
+            return;
+        }
+        playerTwoSequence = [];
+
+        setTimeout(() => {
+            gameTurn();
+        }, 1000)
+        return;
+    } turn = 0;
+}
+
+//handleclick
+function playerOneClick(tile){
+    turn = 1;
+    strikeCounter.innerHTML = "P1";  
+    console.log('p1 click')
+
+    const index = playerOneSequence.push(tile) - 1;
+
+    if (playerOneSequence[index] == sequence[index]) {
+        turn = 2;
+        setTimeout(() => {
+            playerTurn(level);
+        }, 1000)
+        return;
+         
+    } else if (playerOneSequence[index] !== sequence[index]) {
+        strike = true;
+        strikeCounter.innerHTML = "NO!"
+        resetGame();
+        console.log('p1 wrong')
+        
+    } else if (playerOneSequence.length === sequence.length) {
+        if (playerOneSequence.length === 20 && strike == false){
+            resetGame();
+            return;
+        }
+        playerOneSequence = [];
+
+        setTimeout(() => {
+            playerTurn(level);
+            console.log('player turn2')
+        }, 1000)
+        return;
+    } turn = 2;
+}
+
 startButton.addEventListener('click', startGame);
 
+//data-tile is stored in tile variable
+//if value is not empty string handleclick will run with tile as arguement
+tileContainer.addEventListener('click', event => {
+    const {tile} = event.target.dataset;
+
+    if (tile) handleClick(tile);
+});
+
 function startGame(){
+    console.log('start game')
     //all tiles flash?
     gameTurn();
+    
 }
 
 function activateTile(color) {
+    console.log('flash tile')
     const tile = document.querySelector(`[data-tile='${color}']`);
 
     tile.classList.add('activated');
@@ -64,6 +168,7 @@ function playRound(nextSequence) {
 
 //nextStep
 function randomTile() { 
+    console.log('create random tile')
     const tiles = ['green', 'red', 'yellow', 'blue'];
     //randomize selection of tiles
     const random = tiles[Math.floor(Math.random() * tiles.length)];
@@ -74,7 +179,7 @@ function randomTile() {
  //nextRound
 function gameTurn() {
     turn = 0; //set to computer turn
-    level +=1; //increase level
+    level += 1; //increase level
      //players unable to click while its computer turn
     tileContainer.classList.add('unclickable')
 
@@ -83,6 +188,7 @@ function gameTurn() {
         strikeCounter.innerHTML = "WAIT";
     //start next sequence of game
     const nextSequence = [...sequence];
+    console.log('add tile to sequence')
     //returns random tile and add to nextSequence array
     nextSequence.push(randomTile());
     playRound(nextSequence);
@@ -91,63 +197,9 @@ function gameTurn() {
         //total time corresponds to current level * 600ms
         sequence = [...nextSequence];
         setTimeout(() => {
-            playerOne(level);
+            playerTurn(level);
         }, level * 600 + 1000);
-    } 
+    } turn = 1;
+    console.log('compturn')
 }
-
-//human turn
-function playerOne(level) {
-    turn = 1;
-
-    if (turn == 1){
-        strikeCounter.innerHTML = "P1";  
-        tileContainer.classList.remove('unclickable')
-    
-    }
-}
-
-//  function playerOne() {
-// //player ones turn
-// if (p1.turn == true){
-// //allow p1 to follow pattern
-
-//         //if p1 gets pattern wromg
-//         if(p1.sequence != sequence){
-//             //p1 loses the game
-//             strike = true;
-        
-//             //if p1 gets pattern right
-//         } else if (p1.sequence == sequence) {
-//             strike = false;
-//             p1.turn = false;
-//             //set to p2 turn
-//             p2.turn = true;
-//             playerTwo();
-//         }
-//     }
-// }
-
-
-// //player twos turn
-// if (p2.turn == true){
-//     //allow p1 to follow pattern
-    
-//         //if p1 gets pattern wromg
-//         if(p2.sequence != sequence){
-//             //p1 loses the game
-//             strike = true;
-        
-//             //if p1 gets pattern right
-//         } else if (p2.sequence == sequence) {
-//             strike = false;
-//             p2.turn = false;
-//             //set to p2 turn
-//             p1.turn = true;
-//             playerOne();
-//         }
-//     }
-
-//function for sequence
-
 
